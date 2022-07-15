1d50effe6805263f322038247f5e5de1b6de0dcb
Chasity2019 <chasity.spruill@gmail.com>
2022-07-14 23:24:52 -0400

    initial commit

---
+++ IkeaApp3/src/main/java/com/cognixia/IkeaApp3Application.java
@@ -0,0 +1,15 @@
+package com.cognixia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IkeaApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkeaApp3Application.class, args);
+	}
+
+}
---
+++ IkeaApp3/src/main/java/com/cognixia/repository/IkeaApp3Repository.java
@@ -0,0 +1,28 @@
+package com.cognixia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.cognixia.model.Product;
+
+@RepositoryRestResource
+public interface IkeaApp3Repository extends JpaRepository<Product, Integer>{
+	List<Product> findByName(String name);
+	List<Product> findById(int id);
+	List<Product> deleteByRating(int rating);
+
+	@Query("select p from Product p where category LIKE %?1%")
+	List<Product> findByCategory(String category);
+	
+	@Query("select p from Product p where p.code LIKE %?1% and p.inStock is true and p.quantity > 0")
+	List<Product> findByCode(String code);
+	
+	@Query("select p from Product p where rating=10")
+	List<Product> getMaxRating();
+	
+	
+	
+}
